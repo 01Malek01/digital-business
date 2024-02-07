4841fc9bcd9ab13518f9eae56e6284f09c5dd238
Malek <stonemokkaks@gmail.com>
2024-02-07 15:42:28 +0200

    fixed issues

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,13 @@ import Navbar from "@/src/components/navbar/Navbar";
 import "./page.css";
 import Hero from "@/src/components/hero/Hero";
 import BrandingVideo from "@/src/components/brandingVideo/BrandingVideo";
-import OurDiff from "@/src/components/our-difference/OurDiff";
 import { motion, useAnimation } from "framer-motion";
 import HowItWorks from "@/src/components/howItWorks/HowItWorks";
 import WhoWeInvest from "@/src/components/whoWeInvest/WhoWeInvest";
 import Testimonials from "@/src/components/testimonials/Testimonials";
-import Footer from "@/src/components/footer/footer";
 import WhatWeDo from "../components/whatWeDo/WhatWeDo";
+import Footer from "../components/footer/Footer";
+import OurDiff from "../components/our-difference/OurDiff";
 export default function Home() {
   const controls = useAnimation();
   return (
